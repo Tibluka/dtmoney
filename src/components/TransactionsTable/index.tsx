d0551c82ed04dd3c5df64ef997424bfdd247c4fb
Tibluka <cprpc@hotmail.com>
2022-04-22 12:34:37 -0300

    commit

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -38,7 +38,7 @@ export function TransactionsTable() {
                                 {new Intl.NumberFormat('pt-BR', {
                                     style: 'currency',
                                     currency: 'BRL'
-                                }).format(transaction.amount)}
+                                }).format(transaction.amount)} 
                             </td>
                             <td> {transaction.category}</td>
                             <td>
